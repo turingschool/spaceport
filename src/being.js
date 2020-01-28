a0b7fadd5f272a62986e1e9c6daf829408ccad26
Charlie Bandstra <bandstracharlie@gmail.com>
2020-01-28 16:26:08 -0700

    Transferred work to correct repo

--- src/being.js
+++ src/being.js
@@ -1,4 +1,12 @@
 class Being {
+  constructor(name, species) {
+    this.isAlive = true;
+    this.name = name;
+    this.species = species;
+    this.credits = 0;
+  }
+  updateCredits(credits) {
+      this.credits = credits + this.credits;
+  }
 }
-
 module.exports = Being;
