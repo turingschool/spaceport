b0ee74b712c120302d8117c250225933460460ee
Will Mitchell <wvmitchell@gmail.com>
2020-01-28 09:25:50 -0700

    add starter files

---
+++ src/warm-up.js
@@ -0,0 +1,47 @@
+// Read the instructions for each of the warm up exercises carefully. This may
+// seem repititious or pedandtic at first, however it's very important that you
+// become comfortable with these fundamental concepts. Do not move on to
+// iteration 1 until you are confident that you've completed all of iteration 1.
+
+
+//------------------------- Variables -------------------------//
+
+// Initialize two new variables, "name", and "age", and assign values to them with
+// your own name and age.
+
+// var name = "Your name";
+// var ...
+
+
+// Initialize a new variable "someThing", but leave it undefined
+
+
+
+// Initialize a new variable "isHuman", and assign it to a boolean value of your
+// choice
+
+
+
+// Reassign (not initialize) your "age" variable from above. Use a built in math
+// operator to make your age half of what it currently is.
+
+
+
+// Create a variable named "greeting", and assign it to a greeting of your
+// choice. Your chosen greeting must make use of your "name" variable from above
+
+
+
+// Create a new variable named "isMinor", and use a comparison operator to
+// assign a boolean value to this variable. If "age" is less than 18, the
+// variable should be true, otherwise it should be false. You MUST use a
+// comparison operator.
+
+
+
+
+//------------------------- Conditionals -------------------------//
+
+
+// Express the following in code: If the variable age is greater than 100 log
+  // the 
