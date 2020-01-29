c7c008a4df3220bcef2d4dfc7dc974b8f7ee8320
Leah Winters <leah.winters24@gmail.com>
2020-01-29 09:36:28 -0700

    Complete being test

--- src/being.js
+++ src/being.js
@@ -1,4 +1,15 @@
 class Being {
+  constructor(name, species) {
+    this.isAlive = true;
+    this.name = name;
+    this.species = species;
+    this.credits = 0;
+  }
+
+  updateCredits(credit) {
+    this.credits += credit;
+  }
+
 }
 
 module.exports = Being;
