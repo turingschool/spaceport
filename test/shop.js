90ab1daebe57eb9c4b7b5870c38f6e53332493d6
Chris Postma <cjrpostma@gmail.com>
2020-01-29 13:31:59 -0700

    Add shop class

--- test/shop.js
+++ test/shop.js
@@ -1,154 +1,150 @@
-var assert = require('chai').assert;
-var Shop = require('../src/shop');
-var Part = require('../src/part');
-var Being = require('../src/being');
-var Ship = require('../src/ship');
+const { assert } = require('chai');
+const Shop = require('../src/shop');
+const Part = require('../src/part');
+const Being = require('../src/being');
+const Ship = require('../src/ship');
 
 describe('Shop', function() {
-  it.skip('has a name', function() {
-    var newShop = new Shop({name: 'EZ-way'})
+  it('has a name', function() {
+    const newShop = new Shop({ name: 'EZ-way' });
 
-    assert.equal(newShop.name, 'EZ-way')
-  })
+    assert.equal(newShop.name, 'EZ-way');
+  });
 
-  it.skip('can have a different name', function() {
-    var newShop = new Shop({name: 'Conogo'})
+  it('can have a different name', function() {
+    const newShop = new Shop({ name: 'Conogo' });
 
-    assert.equal(newShop.name, 'Conogo')
-  })
+    assert.equal(newShop.name, 'Conogo');
+  });
 
-  it.skip('has no inventory by default', function() {
-    var newShop = new Shop({name: 'Conogo'})
+  it('has no inventory by default', function() {
+    const newShop = new Shop({ name: 'Conogo' });
 
-    assert.deepEqual(newShop.inventory, {})
-  })
+    assert.deepEqual(newShop.inventory, {});
+  });
 
-  it.skip('can add a piece of inventory', function() {
-    var computer = new Part({name: 'comp', type: 'computer', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+  it('can add a piece of inventory', function() {
+    const computer = new Part({ name: 'comp', type: 'computer', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(computer)
+    newShop.addInventory(computer);
 
-    assert.include(Object.values(newShop.inventory), computer)
-  })
+    assert.include(Object.values(newShop.inventory), computer);
+  });
 
-  it.skip('can add another piece of inventory', function() {
-    var hyperdrive = new Part({name: 'Z1', type: 'hyperdrive', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+  it('can add another piece of inventory', function() {
+    const hyperdrive = new Part({ name: 'Z1', type: 'hyperdrive', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(hyperdrive)
+    newShop.addInventory(hyperdrive);
 
-    assert.include(Object.values(newShop.inventory), hyperdrive)
-  })
+    assert.include(Object.values(newShop.inventory), hyperdrive);
+  });
 
-  it.skip('can add multiple pieces of inventory', function() {
-    var computer = new Part({name: 'comp', type: 'computer', value: 200})
-    var hyperdrive = new Part({name: 'hyperdrive', type: 'hyperdrive', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+  it('can add multiple pieces of inventory', function() {
+    const computer = new Part({ name: 'comp', type: 'computer', value: 200 });
+    const hyperdrive = new Part({
+      name: 'hyperdrive',
+      type: 'hyperdrive',
+      value: 200,
+    });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(computer)
-    newShop.addInventory(hyperdrive)
+    newShop.addInventory(computer);
+    newShop.addInventory(hyperdrive);
 
     // remember, if what you're looking at is unfamiliar, look up
     // some documentation!
-    assert.include(Object.values(newShop.inventory), computer)
-    assert.include(Object.values(newShop.inventory), hyperdrive)
-  })
+    assert.include(Object.values(newShop.inventory), computer);
+    assert.include(Object.values(newShop.inventory), hyperdrive);
+  });
 
-  it.skip('can only add parts and food to the inventory', function() {
-    var thing = {name: 'something', type: 'anything'}
-    var otherThing = {name: 'else', quantity: 100}
-    var newShop = new Shop({name: 'Conogo'})
+  it('can only add parts and food to the inventory', function() {
+    const thing = { name: 'something', type: 'anything' };
+    const otherThing = { name: 'else', quantity: 100 };
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(thing)
-    newShop.addInventory(otherThing)
+    newShop.addInventory(thing);
+    newShop.addInventory(otherThing);
 
-    assert.deepEqual(newShop.inventory, {})
-  })
+    assert.deepEqual(newShop.inventory, {});
+  });
 
-  it.skip('cannot outfit a ship without a captian', function() {
-    var fighter = new Ship(
-      { 
-        name: 'Atlantis',
-        type: 'military',
-        maxCrew: 2,
-        odometer: 3340,
-      }
-    )
-    var shell = new Part({name: 'S-1', type: 'shell', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+  it('cannot outfit a ship without a captain', function() {
+    const fighter = new Ship({
+      name: 'Atlantis',
+      type: 'military',
+      maxCrew: 2,
+      odometer: 3340,
+    });
+    const shell = new Part({ name: 'S-1', type: 'shell', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(shell)
-    var result = newShop.outfitShip(fighter, 'shell')
+    newShop.addInventory(shell);
+    const result = newShop.outfitShip(fighter, 'shell');
 
-    assert.equal(result, `cannot outfit a ship without a captian`)
-  })
+    assert.equal(result, `cannot outfit a ship without a captain`);
+  });
 
-  it.skip('cannot outfit a ship if the captian is broke', function() {
-    var captian = new Being('Will', 'human');
+  it('cannot outfit a ship if the captain is broke', function() {
+    const captain = new Being('Will', 'human');
 
-    var fighter = new Ship(
-      { 
-        name: 'Atlantis',
-        type: 'military',
-        captian: captian,
-        maxCrew: 2,
-        odometer: 3340,
-      }
-    )
-    var shell = new Part({name: 'S-1', type: 'shell', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+    const fighter = new Ship({
+      name: 'Atlantis',
+      type: 'military',
+      captain,
+      maxCrew: 2,
+      odometer: 3340,
+    });
+    const shell = new Part({ name: 'S-1', type: 'shell', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(shell)
-    var result = newShop.outfitShip(fighter, 'shell')
+    newShop.addInventory(shell);
+    const result = newShop.outfitShip(fighter, 'shell');
 
-    assert.equal(result, "you require 200 more credits to make this purchase")
-  })
+    assert.equal(result, 'you require 200 more credits to make this purchase');
+  });
 
-  it.skip('cannot outfit a ship if the captian is short funds', function() {
-    var captian = new Being('Will', 'human');
-    captian.credits = 100
+  it('cannot outfit a ship if the captain is short funds', function() {
+    const captain = new Being('Will', 'human');
+    captain.credits = 100;
 
-    var fighter = new Ship(
-      { 
-        name: 'Atlantis',
-        type: 'military',
-        captian: captian,
-        maxCrew: 2,
-        odometer: 3340,
-      }
-    )
-    var shell = new Part({name: 'S-1', type: 'shell', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+    const fighter = new Ship({
+      name: 'Atlantis',
+      type: 'military',
+      captain,
+      maxCrew: 2,
+      odometer: 3340,
+    });
+    const shell = new Part({ name: 'S-1', type: 'shell', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(shell)
-    var result = newShop.outfitShip(fighter, 'shell')
+    newShop.addInventory(shell);
+    const result = newShop.outfitShip(fighter, 'shell');
 
-    assert.equal(result, "you require 100 more credits to make this purchase")
-  })
+    assert.equal(result, 'you require 100 more credits to make this purchase');
+  });
 
-  it.skip('can outfit a ship, and charge the captian', function() {
-    var captian = new Being('Will', 'human');
-    captian.credits = 1000
+  it('can outfit a ship, and charge the captain', function() {
+    const captain = new Being('Will', 'human');
+    captain.credits = 1000;
 
-    var fighter = new Ship(
-      { 
-        name: 'Atlantis',
-        type: 'military',
-        captian: captian,
-        maxCrew: 2,
-        odometer: 3340,
-      }
-    )
-    var shell = new Part({name: 'S-1', type: 'shell', value: 200})
-    var newShop = new Shop({name: 'Conogo'})
+    const fighter = new Ship({
+      name: 'Atlantis',
+      type: 'military',
+      captain,
+      maxCrew: 2,
+      odometer: 3340,
+    });
+    const shell = new Part({ name: 'S-1', type: 'shell', value: 200 });
+    const newShop = new Shop({ name: 'Conogo' });
 
-    newShop.addInventory(shell)
-    var result = newShop.outfitShip(fighter, 'shell')
+    newShop.addInventory(shell);
+    const result = newShop.outfitShip(fighter, 'shell');
 
-    assert.equal(captian.credits, 800)
-    assert.equal(fighter.parts.shell, shell)
-    assert.isUndefined(newShop.inventory.shell)
-    assert.equal(result, `shell added to ship`)
-  })
-})
+    assert.equal(captain.credits, 800);
+    assert.equal(fighter.parts.shell, shell);
+    assert.isUndefined(newShop.inventory.shell);
+    assert.equal(result, `shell added to ship`);
+  });
+});
