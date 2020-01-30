5d68992b0df9a45eb2f45accdae55e93025023c3
Chris Postma <cjrpostma@gmail.com>
2020-01-30 12:56:55 -0700

    Refactor ship class, part class methods

--- src/part.js
+++ src/part.js
@@ -1,22 +1,20 @@
-const validTypes = [
-  'shell',
-  'hyperdrive',
-  'computer',
-  'life support',
-  'landing gear',
-  undefined,
-];
-
 class Part {
   constructor({ name, type, value }) {
     this.name = name;
-    this.type = validTypes.includes(type) ? type : undefined;
+    this.validTypes = [
+      'shell',
+      'hyperdrive',
+      'computer',
+      'life support',
+      'landing gear',
+    ];
+    this.type = this.validTypes.includes(type) ? type : undefined;
     this.value = value;
     this.broken = false;
   }
 
   isValid() {
-    return Boolean(this.name) && Boolean(this.type) && Boolean(this.value);
+    return Boolean(this.name && this.type && this.value);
   }
 }
 
